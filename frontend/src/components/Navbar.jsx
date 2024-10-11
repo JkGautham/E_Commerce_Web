04c7ee6a7a7ec9b352352530d6ee3c0d60cc5e84
JkGautham <jk.gautham@gmail.com>
2024-10-12 00:47:59 +0530

    Navbar implementaton x4 fixed issue with dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,16 +25,22 @@ const Navbar = () => {
         </NavLink>
       </ul>
 
-      <div className="flex item-center gap-6">
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
-        <div className="group-relative">
+      <div className="flex items-center gap-6">
+        <img
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt="Search"
+        />
+
+        {/* Dropdown Menu */}
+        <div className="relative group">
           <img
             src={assets.profile_icon}
             className="w-5 cursor-pointer"
-            alt=""
+            alt="Profile"
           />
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+          <div className="hidden group-hover:block absolute right-0 mt-2 w-36 bg-slate-100 text-gray-500 rounded-lg shadow-lg">
+            <div className="flex flex-col gap-2 py-3 px-5">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Order</p>
               <p className="cursor-pointer hover:text-black">Logout</p>
