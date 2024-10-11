9118af50dc556ba44c33f2bb17580c00a364a130
JkGautham <jk.gautham@gmail.com>
2024-10-11 21:18:54 +0530

    Routing phase 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
