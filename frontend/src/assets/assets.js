3dc102dca0443eb48d8cd0f92c77958cc2c74bde
JkGautham <jk.gautham@gmail.com>
2024-10-12 01:59:34 +0530

    Menu-icon for small devices

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,13 @@
 import logo from "./px695_C_Programming_Language.png";
 import search_icon from "./icons8-search-50.png";
 import profile_icon from "./icons8-person-48.png";
+import cart_icon from "./icons8-cart-96.png";
+import menu_icon from "./icons8-menu-128.png";
 
 export const assets = {
   logo,
   search_icon,
   profile_icon,
+  cart_icon,
+  menu_icon,
 };
