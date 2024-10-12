88d256d278ec871688fe9672869b538646ceace5
JkGautham <jk.gautham@gmail.com>
2024-10-13 03:12:33 +0530

    Done Title,Hero,ShopContext Working on LatestCollection,ProductItem

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,6 +3,7 @@ import search_icon from "./icons8-search-50.png";
 import profile_icon from "./icons8-person-48.png";
 import cart_icon from "./icons8-cart-96.png";
 import menu_icon from "./icons8-menu-128.png";
+import hero_img from "./Image_2.jpg";
 
 export const assets = {
   logo,
@@ -10,4 +11,112 @@ export const assets = {
   profile_icon,
   cart_icon,
   menu_icon,
+  hero_img,
 };
+
+export const products = [
+  {
+    _id: "aaaa",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "bbbb",
+    name: "Women Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716734345448,
+    bestseller: true,
+  },
+  {
+    _id: "cccc",
+    name: "Women Round Neck Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1712634345448,
+    bestseller: true,
+  },
+  {
+    _id: "dddd",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1616634345448,
+    bestseller: true,
+  },
+  {
+    _id: "eeee",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345428,
+    bestseller: true,
+  },
+  {
+    _id: "ffff",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, knitted, pullover shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716674345448,
+    bestseller: true,
+  },
+  {
+    _id: "gggg",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, shirt, close-fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716664345448,
+    bestseller: true,
+  },
+  {
+    _id: "hhhh",
+    name: "Women Round Neck Cotton Top",
+    description:
+      "A lightweight, ussually knitted, pullover shirt, fitting and with soft texture",
+    price: 100,
+    image: [hero_img],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["S", "M", "L"],
+    date: 1716634345548,
+    bestseller: true,
+  },
+];
