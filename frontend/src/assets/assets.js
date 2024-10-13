df025c0f281bf5263f21b728c6d1a033821aa270
JkGautham <jk.gautham@gmail.com>
2024-10-13 18:41:06 +0530

    Implemented Collections Page, Changed ShopContext added some values to assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,10 +3,18 @@ import search_icon from "./icons8-search-50.png";
 import profile_icon from "./icons8-person-48.png";
 import cart_icon from "./icons8-cart-96.png";
 import menu_icon from "./icons8-menu-128.png";
-import hero_img from "./Image_2.jpg";
+import hero_img from "./LadieTop1.jpg";
 import exchange_icon from "./icons8-exchange-50.png";
 import qual_icon from "./icons8-quality-50.png";
 import support_icon from "./icons8-support-50.png";
+import dropdown_icon from "./icons8-dropdown-50.png";
+
+import kidtop from "./KidTop.jpg";
+import kidbtm from "./KidBottom.jpg";
+import mentop from "./MenTop.jpg";
+import menbtm from "./MenBottom.jpg";
+import wmntop from "./LadieTop.jpg";
+import wmnbtm from "./LadieBottom.jpg";
 
 export const assets = {
   logo,
@@ -18,6 +26,7 @@ export const assets = {
   exchange_icon,
   qual_icon,
   support_icon,
+  dropdown_icon,
 };
 
 export const products = [
@@ -27,7 +36,7 @@ export const products = [
     description:
       "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
     price: 100,
-    image: [hero_img],
+    image: [wmntop],
     category: "Women",
     subCategory: "Topwear",
     sizes: ["S", "M", "L"],
@@ -36,25 +45,25 @@ export const products = [
   },
   {
     _id: "bbbb",
-    name: "Women Neck Cotton Top",
+    name: "Women Denim Cotton Bottom",
     description:
       "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
+    price: 200,
+    image: [wmnbtm],
     category: "Women",
-    subCategory: "Topwear",
+    subCategory: "Bottomwear",
     sizes: ["S", "M", "L"],
     date: 1716734345448,
     bestseller: true,
   },
   {
     _id: "cccc",
-    name: "Women Round Neck Top",
+    name: "Men Round Neck Top",
     description:
       "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
-    category: "Women",
+    price: 120,
+    image: [mentop],
+    category: "Men",
     subCategory: "Topwear",
     sizes: ["S", "M", "L"],
     date: 1712634345448,
@@ -62,25 +71,25 @@ export const products = [
   },
   {
     _id: "dddd",
-    name: "Women Round Neck Cotton Top",
+    name: "Men Round Cotton Pant",
     description:
       "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
-    category: "Women",
-    subCategory: "Topwear",
+    price: 180,
+    image: [menbtm],
+    category: "Men",
+    subCategory: "Bottomwear",
     sizes: ["S", "M", "L"],
     date: 1616634345448,
     bestseller: false,
   },
   {
     _id: "eeee",
-    name: "Women Round Neck Cotton Top",
+    name: "Kid Round Neck Cotton Top",
     description:
       "A lightweight, ussually knitted, pullover shirt, close-fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
-    category: "Women",
+    price: 210,
+    image: [kidtop],
+    category: "Kid",
     subCategory: "Topwear",
     sizes: ["S", "M", "L"],
     date: 1716634345428,
@@ -88,23 +97,23 @@ export const products = [
   },
   {
     _id: "ffff",
-    name: "Women Round Neck Cotton Top",
+    name: "Kid Round Jute Jeans",
     description:
       "A lightweight, knitted, pullover shirt, close-fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
-    category: "Women",
-    subCategory: "Topwear",
+    price: 85,
+    image: [kidbtm],
+    category: "Kid",
+    subCategory: "Bottomwear",
     sizes: ["S", "M", "L"],
     date: 1716674345448,
     bestseller: false,
   },
   {
     _id: "gggg",
-    name: "Women Round Neck Cotton Top",
+    name: "Women Square Cotton Top",
     description:
       "A lightweight, ussually knitted, shirt, close-fitting and with soft texture",
-    price: 100,
+    price: 250,
     image: [hero_img],
     category: "Women",
     subCategory: "Topwear",
@@ -114,12 +123,12 @@ export const products = [
   },
   {
     _id: "hhhh",
-    name: "Women Round Neck Cotton Top",
+    name: "Women Elipse Cotton Top",
     description:
       "A lightweight, ussually knitted, pullover shirt, fitting and with soft texture",
-    price: 100,
-    image: [hero_img],
-    category: "Women",
+    price: 140,
+    image: [mentop],
+    category: "Men",
     subCategory: "Topwear",
     sizes: ["S", "M", "L"],
     date: 1716634345548,
