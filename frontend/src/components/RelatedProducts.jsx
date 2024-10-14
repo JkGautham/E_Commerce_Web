89289c82e42685c15c52e0ca8aadc280a6528fbf
JkGautham <jk.gautham@gmail.com>
2024-10-14 20:02:16 +0530

    Related Suggestions done

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -2,19 +2,24 @@ import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useEffect } from "react";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const RelatedProducts = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
   const [related, setRelated] = useState([]);
   useEffect(() => {
     if (products.length > 0) {
-      let productsCopy = products.slice();
-      console.log(productsCopy); // 9 items present
-      productsCopy = productsCopy.filter((item) => category === item.category);
-      productsCopy = productsCopy.filter(
+      let productsCopy1 = products.slice();
+      let productsCopy2 = products.slice();
+      productsCopy1 = productsCopy1.filter(
+        (item) => category === item.category
+      );
+      productsCopy2 = productsCopy2.filter(
         (item) => subCategory === item.subCategory
       );
-      console.log(productsCopy); // 0 items present instead of 3 items
+      setRelated(productsCopy1.slice(0, 5));
+      setRelated(productsCopy2.slice(0, 5));
+      console.log(related);
     }
   }, [products]);
 
@@ -22,6 +27,17 @@ const RelatedProducts = ({ category, subCategory }) => {
     <div className="my-24">
       <div className="text-center text-3xl py-2">
         <Title text1={"RELATED"} text2={"PRODUCTS"} />
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
     </div>
   );
