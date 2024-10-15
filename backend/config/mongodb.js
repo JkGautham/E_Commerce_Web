e6fb89c6384930e0c73433adc3d1ca08f8691369
JkGautham <jk.gautham@gmail.com>
2024-10-16 01:55:36 +0530

    Implementing Backend x3

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   mongoose.connection.on("connected", () => console.log("DB Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI_GJ}`);
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 };
 
 export default connectDb;
