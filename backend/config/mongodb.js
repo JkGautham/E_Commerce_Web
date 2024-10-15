cee2f82a02b5dd2506ab3718dc6eaaab8d21b35e
JkGautham <jk.gautham@gmail.com>
2024-10-15 08:46:42 +0530

    Backend Connections to mongo, cloudinary estd

---
+++ backend/config/mongodb.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  mongoose.connection.on("connected", () => console.log("DB Connected"));
+  await mongoose.connect(`${process.env.MONGODB_URI_GJ}`);
+};
+
+export default connectDb;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+//api end point
+app.get("/", (req, res) => res.send("API Working"));
+app.listen(port, () => console.log("server started at " + port));
+connectDb();
+connectCloudinary();
